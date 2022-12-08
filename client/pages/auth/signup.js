ee0025c67c9467689103c838e50f1b63431fc69f
Nurettin Şen <nurie487@gmail.com>
2022-12-08 21:36:12 +0300

    Updatep roject

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { Button, message, Space } from "antd";
 
 const schema = yup
   .object({
@@ -23,28 +24,56 @@ export default () => {
     resolver: yupResolver(schema),
   });
 
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const errorMsgFn = (msg) => {
+    messageApi.open({
+      type: "error",
+      content: msg,
+    });
+  };
+
   const onSubmit = async (data) => {
-    const response = await axios.post("/api/users/signup", data);
+    try {
+      const response = await axios.post("/api/users/signup", data);
+      console.log(response.data);
+    } catch (error) {
+      if (error.response.status === 400) {
+        errorMsgFn(error.response.data.errors[0].message); // şimdilik böyle proje cesıtlendıkce array'i dönmek ıcın dusunuruz.
+      }
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <h1>Sign Up</h1>
-      <div className="form-group">
-        <label>Email Address</label>
-        <input {...register("email")} className="form-control" />
-      </div>
-      <div>{errors.email?.message}</div>
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          {...register("password")}
-          type="password"
-          className="form-control"
-        />
-      </div>
-      <div>{errors.password?.message}</div>
-      <button className="btn btn-primary">Sign Up</button>
-    </form>
+    <>
+      {contextHolder}
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Sign Up</h1>
+        <div className="form-group">
+          <label>Email Address</label>
+          <input {...register("email")} className="form-control" />
+        </div>
+
+        <div>
+          <p>{errors.email?.message}</p>
+        </div>
+
+        <div className="form-group">
+          <label>Password</label>
+          <input
+            {...register("password")}
+            type="password"
+            className="form-control"
+          />
+        </div>
+
+        <div>
+          <p>{errors.password?.message}</p>
+        </div>
+
+        <button className="btn btn-primary">Sign Up</button>
+      </form>
+    </>
   );
 };
