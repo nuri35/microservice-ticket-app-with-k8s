c3c5d00f1bf28aaad60fc70eeb1c1818397baac0
Nurettin Şen <nurie487@gmail.com>
2023-01-07 18:14:55 +0300

    Add publisher class

--- nats-test/src/events/ticket-created-listener.ts
+++ nats-test/src/events/ticket-created-listener.ts
@@ -7,7 +7,7 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subject.TicketCreated = Subject.TicketCreated;
   queueGroupName = "payment-service";
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     console.log("Event data!", data);
     msg.ack();
   }
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,20 +1,24 @@
 import nats from "node-nats-streaming";
+import { TicketCreatedPublisher } from "./events/ticket-created-publisher";
 console.clear();
 const stan = nats.connect("ticketing", "abc", {
   url: "http://localhost:4222",
 });
 
-stan.on("connect", () => {
+stan.on("connect", async () => {
   // Simple Publisher (all publishes are async in the node version of the client)
   console.log("Publisher connected to Nats");
 
-  const data = JSON.stringify({
+  const data = {
     id: "123",
     title: "concert",
     price: 20,
-  });
+  };
 
-  stan.publish("ticket:created", data, () => {
-    console.log("event published data");
-  });
+  const publisher = new TicketCreatedPublisher(stan);
+  try {
+    await publisher.publish(data);
+  } catch (error) {
+    console.log(error);
+  }
 });
