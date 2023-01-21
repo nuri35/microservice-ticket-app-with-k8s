2e8eb3cf409985fb1b1fea56d078e689512b296b
Nurettin Şen <nurie487@gmail.com>
2023-01-21 17:14:53 +0300

    Updated reserve check ticket

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -8,6 +8,8 @@ import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
 import { updateTicketRouter } from "./routes/update";
 import { natsWrapper } from "./nats-wrapper";
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
+import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
 
 const app = express();
 app.set("trust proxy", true);
@@ -65,6 +67,9 @@ const start = async () => {
 
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI);
     console.log("connect success to database");
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -5,6 +5,7 @@ import {
   NotFoundError,
   requireAuth,
   NotAuthorizedError,
+  BadRequestError,
 } from "@fbticketss/common";
 import { Ticket } from "../models/tickets";
 import { TicketUpdatedPublisher } from "../events/publisher/ticket-updated-publisher";
@@ -27,6 +28,10 @@ router.put(
 
     if (!ticket) {
       throw new NotFoundError();
+    }
+
+    if (ticket.orderId) {
+      throw new BadRequestError("Cannot edit a reserved ticket");
     }
 
     if (ticket.userId !== req.currentUser!.id) {
